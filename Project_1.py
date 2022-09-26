6188fe1ed6ae0d9a2825e48ea24ccb9368da2697
jaredmurri <113555740+jaredmurri@users.noreply.github.com>
2022-09-26 14:13:18 -0600

    Fixed diameter, updated x-direction, redef force

--- Project_1.py
+++ Project_1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy
 import math
+import scipy.integrate._quadrature as integrate
 
 ##### Givens  #####
 density= 1153
@@ -8,18 +9,20 @@ kin_viscos= 0.008
 L1=1
 L2=4.5
 L3=0.5
+D3=4
+D1=1
 outer_width=0.07620
 inner_width=0.07493
 weight=10000
 
 
 def diameter (x):
-    if x < 1:
-        return 1
-    elif x>4.5:
-        return 4
+    if x <= L3:
+        return D3
+    elif x>=L3+L2:
+        return D1
     else:
-        return (4/4.5)*(x-1)
+        return D3-((D3-D1)/L2)*(x)
 
 def velocity(x):
     return 92/(diameter(x)**2)
@@ -30,7 +33,6 @@ def Reynolds_number (x):
 def drag_coeff(x):
     return 0.027/(Reynolds_number(x))**(1/7)
 
-def Force_per_unit_length(x0,x1):
-    import scipy.integrate._quadrature as integrate
-    integrand= lambda x: .5*drag_coeff*density*velocity**2
-    integrate(integrand,x0,x1)
+def Force_per_unit_length(x):
+    perimeter=outer_width*4
+    return perimeter*.5*drag_coeff(x)*density(x)*(velocity(x))**2
